2363e569fc4a672a0b897585b2b7ad19d4036ddc
mcpereira <maria.pereira@criticalsoftware.com>
2018-09-18 16:43:15 +0100

    NEED CHANGES - INCOMPLETE

--- src/main/java/com/calculateVersion2/exercise/calculateVersion2/ficheiroCSV/Ficheiro.java
+++ src/main/java/com/calculateVersion2/exercise/calculateVersion2/ficheiroCSV/Ficheiro.java
@@ -26,4 +26,6 @@ public class Ficheiro {
 		reader.close();	
 	}
 	
+	
+	
 }
--- src/main/java/com/calculateVersion2/exercise/calculateVersion2/operations/CalculateVersion2Service.java
+++ src/main/java/com/calculateVersion2/exercise/calculateVersion2/operations/CalculateVersion2Service.java
@@ -5,5 +5,11 @@ package com.calculateVersion2.exercise.calculateVersion2.operations;
  */
 
 public class CalculateVersion2Service {
-
+		private CalculateVersion2 values;
+		
+		public void setValues (CalculateVersion2 vl) {
+			values = vl;
+		}
+		
+		
 }
