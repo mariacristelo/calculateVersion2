c699a06ac08cd2120dbf24c5fdd3b595d7d25450
mcpereira <maria.pereira@criticalsoftware.com>
2018-09-18 12:12:22 +0100

    Add project maven with java and springBoot 2.0.5

---
+++ src/main/java/com/calculateVersion2/exercise/calculateVersion2/CalculateVersion2Application.java
@@ -0,0 +1,12 @@
+package com.calculateVersion2.exercise.calculateVersion2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateVersion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateVersion2Application.class, args);
+	}
+}
