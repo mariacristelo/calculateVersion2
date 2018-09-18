97af5954e97dc0b60b817ae5a06cba665ecb0ee3
mcpereira <maria.pereira@criticalsoftware.com>
2018-09-18 12:26:46 +0100

    Add class com os parametros - value1 [double], value2 [double] e operation [string]

---
+++ src/main/java/com/calculateVersion2/exercise/calculateVersion2/operations/calculateVersion2.java
@@ -0,0 +1,20 @@
+package com.calculateVersion2.exercise.calculateVersion2.operations;
+
+public class calculateVersion2 {
+	private double value1;
+	private double value2;
+	private String op;
+	
+	public calculateVersion2() {
+		super();
+	}
+	
+	public calculateVersion2(double value1, double value2, String op) {
+		super();
+		this.value1 = value1;
+		this.value2 = value2;
+		this.op = op;
+	}
+	
+	
+}
